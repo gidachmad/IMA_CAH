52bdd0c40cddcd99d068d355b48fdd10925aae40
Gid AAf <gaaf0809@gmail.com>
2020-09-16 12:50:37 +0700

    jurnal kedua

---
+++ jurnal-02/jurnal1.java
@@ -0,0 +1,9 @@
+class jurnal1{
+
+  public static void main(String[] args){
+    System.out.println("Nama : Gid Achmad Ahlul F");
+    System.out.println("NIM : 6706203061");
+    System.out.println("Kelas : D3IF-44-02");
+  }
+
+}
